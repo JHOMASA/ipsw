9eeb117f60e7c9b72a99e72fc00c1a2adffc32ff
jhonatan Salas <128430511+JHOMASA@users.noreply.github.com>
2025-06-06 23:06:25 -0500

    registration.py

--- app/registration.py
+++ app/registration.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from datetime import datetime
 from app.inventory_logic import InventoryCalculator
+from app.database import InventoryDB
 
 class RegistrationSystem:
     def __init__(self, db):
